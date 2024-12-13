5c839bb9000ce65c8043d89dc130968a63873c50
asa99-cpu <58121161+asa99-cpu@users.noreply.github.com>
2024-12-13 19:07:54 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,25 @@
 import streamlit as st
-from utils import add_client_tab, client_overview_tab
+from utils import load_database, add_client_tab, client_overview_tab
 from sidebar import add_sidebar
-from contact_info import display_contact_info
+import start  # Import the start.py file
 
 def main():
-    st.title("Clinic App")
-
-    # Add sidebar for navigation
-    add_sidebar()
-
-    # Check the selected section from session state
+    """Main function to run the app."""
+    # Display the welcome animation first
     if "selected_section" not in st.session_state:
-        st.session_state["selected_section"] = "Add Client"  # Default section
+        st.session_state["selected_section"] = "Start Page"
 
-    if st.session_state["selected_section"] == "Add Client":
-        add_client_tab()
-    elif st.session_state["selected_section"] == "Client Overview":
-        client_overview_tab()
-    elif st.session_state["selected_section"] == "Contact Info":
-        display_contact_info()
+    if st.session_state["selected_section"] == "Start Page":
+        start.display_welcome_animation()
+    else:
+        add_sidebar()  # Sidebar navigation
+        # Show the content based on selected section
+        if st.session_state["selected_section"] == "Add Client":
+            add_client_tab()
+        elif st.session_state["selected_section"] == "Client Overview":
+            client_overview_tab()
+        elif st.session_state["selected_section"] == "Contact Info":
+            st.write("Contact Info Placeholder")
 
 if __name__ == "__main__":
     main()
