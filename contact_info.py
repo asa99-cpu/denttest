ad156d2aa604f7657eeb3cf7df1a08e3ca7cf443
asa99-cpu <58121161+asa99-cpu@users.noreply.github.com>
2024-12-13 18:02:25 +0300

    Update contact_info.py

--- contact_info.py
+++ contact_info.py
@@ -4,13 +4,10 @@ import streamlit as st
 def display_contact_info():
     st.subheader("Contact Info")
     
-    # Print the current working directory to verify
-    st.write("Current Working Directory:", os.getcwd())
-    
-    # Define the correct path to the image
+    # Define the correct relative path to the image
     image_path = os.path.join("denttest", "content", "denta.JPG")
     
-    # Try to load and display the image
+    # Display the image
     try:
         st.image(image_path, width=200)  # Adjust the width if necessary
     except Exception as e:
