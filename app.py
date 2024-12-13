8cd618613a823711198ff8f49a1f1332cfdc6920
asa99-cpu <58121161+asa99-cpu@users.noreply.github.com>
2024-12-13 18:59:36 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,23 @@
 import streamlit as st
+from utils import add_client_tab, client_overview_tab
 from sidebar import add_sidebar
-from utils import load_database, add_client_tab, client_overview_tab
 from contact_info import display_contact_info
 
-# Initialize session state for selected_section if it does not exist
-if "selected_section" not in st.session_state:
-    st.session_state["selected_section"] = "Add Client"  # Default section
-
 def main():
-    # Sidebar navigation
+    st.title("Clinic App")
+
+    # Add sidebar for navigation
     add_sidebar()
 
-    # Display the correct section based on the selected section
-    selected_section = st.session_state["selected_section"]
-    
-    if selected_section == "Add Client":
+    # Check the selected section from session state
+    if "selected_section" not in st.session_state:
+        st.session_state["selected_section"] = "Add Client"  # Default section
+
+    if st.session_state["selected_section"] == "Add Client":
         add_client_tab()
-    elif selected_section == "Client Overview":
+    elif st.session_state["selected_section"] == "Client Overview":
         client_overview_tab()
-    elif selected_section == "Contact Info":
+    elif st.session_state["selected_section"] == "Contact Info":
         display_contact_info()
 
 if __name__ == "__main__":
