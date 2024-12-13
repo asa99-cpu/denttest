b46d7edae00914af6bff838a405eedad7c49e125
asa99-cpu <58121161+asa99-cpu@users.noreply.github.com>
2024-12-13 18:11:27 +0300

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from utils import add_client, load_database
 from contact_info import display_contact_info  # Import the function for contact info
 
 def main():
-    st.title("Clinic App")
+    st.title("کلینیکی ددانی شادیار")
 
     # Sidebar for navigation
     add_sidebar()  # This will call the function from sidebar.py
